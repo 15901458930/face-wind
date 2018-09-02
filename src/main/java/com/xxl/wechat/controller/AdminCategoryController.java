960fb8ce9b69d5b51921f31153b97015f462f826
13712889877 <38270130+13712889877@users.noreply.github.com>
2018-09-02 16:25:26 +0800

    category by cg

--- src/main/java/com/xxl/wechat/controller/AdminCategoryController.java
+++ src/main/java/com/xxl/wechat/controller/AdminCategoryController.java
@@ -1,6 +1,28 @@
 package com.xxl.wechat.controller;
 
 import com.jfinal.core.Controller;
+import com.xxl.wechat.model.generator.SySubCategory;
+import com.xxl.wechat.service.CategoryService;
+import com.xxl.wechat.vo.LayuiResultVO;
 
 public class AdminCategoryController extends Controller {
+
+    static CategoryService categoryService = new CategoryService();
+
+    public void index() {
+        render("admin-category-list.html");
+    }
+
+    public void list() {
+        String name = getPara("name");
+        String mainCategoryId = getPara(0);
+        String page = getPara("page");
+        String limitStr = getPara("limit");
+        int curPage = (page == null) ? 1 : Integer.parseInt(page);
+        int limit = (limitStr == null) ? 10 : Integer.parseInt(limitStr);
+        LayuiResultVO<SySubCategory> SySubCategory = categoryService.findAllSubCategory(curPage, limit, name, mainCategoryId);
+
+        renderJson(SySubCategory);
+    }
+
 }
---
+++ src/main/webapp/js/admin/category/admin-category-list.js
@@ -0,0 +1,169 @@
+layui.use(['table','jquery','layer','form'], function(){
+    var form = layui.form;
+    var $ = jQuery = layui.$;
+    var table = layui.table;
+    var layer = layui.layer;
+
+    var name = $("input[name='name']").val();
+    var url = "/admin/category/list?name"+name;
+
+    //第一个实例
+    var tabins = table.render({
+        elem: '#category-list'
+        ,url: url //数据接口
+        ,page: true //开启分页
+        ,cols: [[ //表头
+            {field: 'ID', title: 'ID', width:'10%', sort: true, fixed: 'left'},
+            ,{field: 'NAME',  width:'20%',title: '物品名称'}
+            ,{field: 'SHORT_NAME', title: '物品简称' ,width:'15%'}
+            ,{field: 'MAIN_CATEGORY_NAME',title: '物品类型', width:'15%'}
+            ,{field: 'CREATE_DATE', title: '创建时间', width:'20%'}
+            ,{field: 'DO', title: '操作',toolbar: '#user-operation', width:'20%'}
+        ]],
+        loading:true
+    });
+
+    $("#search-btn").click(function(){
+        var name = $("input[name='name']").val();
+        var url = "/admin/user/list?name="+name;
+        tabins.reload({url:url,page:1});
+    });
+
+    //监听工具条
+    table.on('tool(category-filter)', function(obj){ //注：tool是工具条事件名，test是table原始容器的属性 lay-filter="对应的值"
+        var data = obj.data; //获得当前行数据
+        var layEvent = obj.event; //获得 lay-event 对应的值
+        var tr = obj.tr; //获得当前行 tr 的DOM对象
+
+        if(layEvent === 'detail'){ //查看
+
+            fillData("#category-detail-window",data);
+
+            //打开编辑框
+            var openDetail = layer.open({
+                type:1,
+                title:'详细信息',
+                content:$("#category-detail-window"),
+                area:['600px', '500px'],
+                anim: 3
+            });
+
+            //do somehing
+        } else if(layEvent === 'del'){ //删除
+            layer.confirm('确定删除该条记录吗？', function(index){
+                del(data.ID,function () {
+                    obj.del(); //删除对应行（tr）的DOM结构，并更新缓存
+                    layer.close(index);
+                    layer.msg('删除成功');
+                },function(){
+                    layer.close(index);
+                    layer.msg('删除失败，请稍后重试');
+                });
+            });
+        } else if(layEvent === 'edit'){ //编辑
+
+            //打开编辑框
+            var openEdit = layer.open({
+                type:1,
+                title:'详细信息',
+                content:$("#category-edit-window"),
+                area:['600px', '500px'],
+                anim: 2
+            });
+
+            fillData("#category-edit-window",data);
+
+            //表单初始赋值
+            form.val('category-form-filter', {
+                "userType":data.USER_TYPE
+            });
+
+
+            //其它预览数据赋值
+
+
+
+            //监听提交
+            form.on('submit(save-category)', function(record){
+                var userType = record.field.userType;
+
+                var userTypeName = $("#edit-category-type").find("option:selected").text();
+
+                var id = data.ID;
+
+                changeStatus(id,userType,function () {
+                    layer.msg('修改成功');
+                    obj.update({
+                        USER_TYPE:userType,
+                        USER_TYPE_NAME:userTypeName
+                    });
+                    layer.close(openEdit)
+
+                },function () {
+                    layer.msg('修改失败，请稍后重试');
+                });
+
+                return false;
+            });
+        }
+    });
+
+    function fillData(selector,data){
+
+
+        $(selector).find(".id").text(data.ID);
+        $(selector).find(".name").text(data.NAME);
+        $(selector).find(".shortName").text(data.SHORT_NAME);
+        $(selector).find(".createDate").text(data.CREATE_DATE);
+        $(selector).find(".mainCategoryName").text(data.MAIN_CATEGORY_ID);
+    }
+
+
+
+    /**
+     * 删除
+     * @param id
+     */
+    function del(id,suc,fail){
+        $.ajax({
+            type: "get",
+            dataType: 'json',
+            url: "/admin/category/del/"+id,
+            success: function (jsonObj) {
+                if(jsonObj.success===true){
+                    suc();//回调，先从数据库删除成功以后再删除列
+                }else{
+                    fail();
+                }
+            },error:function () {
+                fail();
+            }
+
+        });
+    }
+
+    /**
+     * 删除
+     * @param id
+     */
+    function changeStatus(id,userType,suc,fail){
+        $.ajax({
+            type: "get",
+            dataType: 'json',
+            url: "/admin/category/change/"+id+"-"+userType,
+            success: function (jsonObj) {
+                if(jsonObj.success===true){
+                    suc();//回调，先从数据库删除成功以后再删除列
+                }else{
+                    fail();
+                }
+            },error:function () {
+                fail();
+            }
+
+        });
+    }
+
+
+});
+
